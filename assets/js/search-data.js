d76a8ef9361dca7201820070c5c10120669a86d9
shaohaibin <78459839+shaohaibin@users.noreply.github.com>
2025-08-26 04:36:35 +0000

    Deploying to gh-pages from @ shaohaibin/shaohaibin.github.io@617b4353124746977bc22929bae8d07f5e6a6b9e 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -459,14 +459,14 @@ ninja.data = [{
           description: "USV Swarm",
           section: "Projects",handler: () => {
               window.location.href = "/projects/4_project/";
-            },},{id: "projects-swarm-responsiveness-enhancement-via-neighbor-selection",
-          title: 'Swarm Responsiveness Enhancement via Neighbor Selection',
+            },},{id: "projects-fast-consensus-via-neighbor-selection",
+          title: 'Fast Consensus via Neighbor Selection',
           description: "UGV Swarm",
           section: "Projects",handler: () => {
               window.location.href = "/projects/5_project/";
-            },},{id: "projects-project-6",
-          title: 'project 6',
-          description: "a project with no image",
+            },},{id: "projects-",
+          title: '',
+          description: "",
           section: "Projects",handler: () => {
               window.location.href = "/projects/6_project/";
             },},{id: "projects-higher-order-interaction-networks",
