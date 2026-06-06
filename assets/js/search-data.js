51bc3692e3899027ee7d5d0b86348bc049455b4b
shaohaibin <78459839+shaohaibin@users.noreply.github.com>
2026-06-06 08:19:29 +0000

    Deploying to gh-pages from @ shaohaibin/shaohaibin.github.io@408bff32d04a62a62e5c8b3f125026d7f0b69c2b 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -32,7 +32,7 @@ ninja.data = [{
           },
         },{id: "nav-people",
           title: "People",
-          description: "members of the lab",
+          description: "",
           section: "Navigation",
           handler: () => {
             window.location.href = "/people/";
