827eb7b10d500a1606932710557b083fb07550f8
shaohaibin <78459839+shaohaibin@users.noreply.github.com>
2026-06-06 05:25:42 +0000

    Deploying to gh-pages from @ shaohaibin/shaohaibin.github.io@8f05ccc83f910802853333fec6c411fe0b1f998c 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -11,7 +11,7 @@ ninja.data = [{
     },
   },{id: "nav-",
           title: "",
-          description: "",
+          description: "Swarm Intelligence Laboratory",
           section: "Navigation",
           handler: () => {
             window.location.href = "/projects/";
